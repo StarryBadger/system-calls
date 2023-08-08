8db8c04a68d6ff9119be1eb57af15849808da93e
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2023-08-08 21:02:08 +0530

    File input

--- Part 1/main.c
+++ Part 1/main.c
@@ -1,17 +1,33 @@
 #include "main.h"
 
-int main(int argc, char** argv) {
-
+int main(int argc, char **argv)
+{
     /*
     You are expected to make use of File Management
     syscalls for this part.
-    
+
     **INPUT**
     Filename will be given as input from the command line.
     */
 
     // Write your code here
-    
 
+    char filename[1000], newfilename[1000 + 8];
+    char rev[9] = "_reverse";
+    scanf("%s", filename);
+
+    int length = strlen(filename), i, j;
+    for (i = length - 1; i >= 0; i--)
+        if (filename[i] == '.')
+            break;
+    if (i == -1)
+        i = length;
+    int c = 0;
+    for (j = 0; j < i; j++)
+    {
+        newfilename[c++] = filename[j];
+    }
+
+    printf("%s", newfilename);
     return 0;
 }
--- Part 1/main.h
+++ Part 1/main.h
@@ -4,3 +4,5 @@ Include all the header files here.
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include <sys/stat.h>
+#include <sys/types.h>
